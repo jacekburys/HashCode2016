71e15664957c85d64c5f1be4f6b5b441ce932019
Jonathan King <jonathanlking@me.com>
2016-02-11 18:57:11 +0000

    Create Drone class

---
+++ src/com/unicorn/Drone.java
@@ -0,0 +1,32 @@
+package com.unicorn;
+
+import java.util.List;
+
+public class Drone {
+    final int id;
+    final int maximumLoad;
+    private Location location;
+    private List<Product> products;
+
+    public Drone(int id, int maximumLoad, List<Product>, Location location) {
+        this.id = id;
+        this.maximumLoad = maximumLoad;
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+}
