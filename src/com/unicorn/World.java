40f42c97097727aaa6157ad7882f02bf274fe057
Jacek Burys <jsb314@ic.ac.uk>
2016-02-11 19:14:20 +0000

    world

--- src/com/unicorn/World.java
+++ src/com/unicorn/World.java
@@ -1,7 +1,20 @@
 package com.unicorn;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by jacek on 11/02/16.
  */
 public class World {
+
+    private List<Order> orders = new ArrayList<>();
+    private List<Warehouse> warehouses = new ArrayList<>();
+    private List<Drone> drones = new ArrayList<>();
+
+    public World(List<Warehouse> warehouses, List<Drone> drones, List<Order> orders) {
+        this.warehouses = warehouses;
+        this.drones = drones;
+        this.orders = orders;
+    }
 }
