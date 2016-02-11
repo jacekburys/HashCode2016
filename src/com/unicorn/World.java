13f49c1b1e017bd3439ff9590aa108fb9e6187c7
Jacek Burys <jsb314@ic.ac.uk>
2016-02-11 19:53:29 +0000

    getWarehouse

--- src/com/unicorn/World.java
+++ src/com/unicorn/World.java
@@ -18,7 +18,33 @@ public class World {
         this.orders = orders;
     }
 
+    private Warehouse getWarehouseWith(List<Product> products) {
+        int m = 0;
+        Warehouse res = warehouses.get(0);
+
+        for(int i=0; i<warehouses.size(); i++) {
+            int c = 0;
+            for(int j=0; j<products.size(); j++) {
+               // TODO
+            }
+            if(c > m) {
+                res = warehouses.get(i);
+                m = c;
+            }
+        }
+
+        return res;
+    }
+
     public void schedule() {
+
+        if(orders.isEmpty()) return;
+        if(drones.isEmpty()) return;
+
+        Order order = orders.get(0);
+        Drone drone = drones.get(0);
+
+        Warehouse warehouse = getWarehouseWith(order.getProducts());
 
     }
 }
